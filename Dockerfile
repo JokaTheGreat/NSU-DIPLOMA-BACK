0c1a3cdc306159367cf9b271239e37ebd1fa4c41
Nikita Latysh <n.latysh@g.nsu.ru>
2022-12-04 13:13:47 +0700

    Test paramiko

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,5 @@ EXPOSE 3333
 
 RUN pip install flask
 RUN pip install -U flask-cors
+RUN pip install paramiko
 CMD ["python3", "main.py"]
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import subprocess
 import xml.etree.ElementTree as ET
 import re
+import paramiko
 
 from flask import Flask, request
 from flask_cors import cross_origin
 
 host = '0.0.0.0'
 port = 3333
+
+sshHost = 'ftp_storage'
+sshUser = 'user'
+sshPassw = 'user'
 
 app = Flask(__name__)
 
@@ -17,8 +22,21 @@ def update_pick_times():
     eventId = request.json["eventId"]
     picks = request.json["picks"]
 
+    sshClient = paramiko.SSHClient()
+    sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    sshClient.connect(hostname=sshHost, username=sshUser, password=sshPassw)
+    stdin, stdout, stderr = sshClient.exec_command('su root')
+    stdin.write("root" + "\n")
+    stdin.flush()
+    stdin, stdout, stderr = sshClient.exec_command(f"scxmldump -fpP -E {eventId} -o TEST.xml \
+         -d postgresql://sysop:sysop@localhost/seiscomp")
+
+    return request.json
+
+
     getXmlFromDBCommand = f"scxmldump -fpP -E {eventId} -o {eventId}.xml \
          -d postgresql://sysop:sysop@localhost/seiscomp"
+    # TODO: eventId.xml Заменить на evenId после последнего слэша.xml
 
     process = subprocess.Popen(getXmlFromDBCommand, stdout=subprocess.PIPE)
     process.wait()
