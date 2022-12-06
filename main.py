fa731447a4d7af3075db533af5f4e97c1fde9b9f
Nikita Latysh <n.latysh@g.nsu.ru>
2022-12-06 22:37:19 +0700

    Add creating picks that doesn't exists logic

--- main.py
+++ main.py
@@ -24,7 +24,36 @@ def dump_xml_from_db(ssh_client, event_id):
     stdout.channel.recv_exit_status()
 
 
-def update_xml(ssh_client, picks):
+def create_new_xml_pick(pick_id, phase, time, network, station, channel):
+    new_pick = ET.Element("pick")
+    new_pick.set("publicID", str(pick_id))
+    pick_phase = ET.SubElement(new_pick, "phaseHint")
+    pick_phase.text = str(phase)
+    pick_polarity = ET.SubElement(new_pick, "polarity")
+    pick_polarity.text = "undecidable"
+    pick_evaluation_mode = ET.SubElement(new_pick, "evaluationMode")
+    pick_evaluation_mode.text = "manual"
+    pick_time = ET.SubElement(new_pick, "time")
+    pick_time_value = ET.SubElement(pick_time, "value")
+    pick_time_value.text = str(time)
+    pick_waveform_id = ET.SubElement(new_pick, "waveformID")
+    pick_waveform_id.set("networkCode", str(network))
+    pick_waveform_id.set("stationCode", str(station))
+    pick_waveform_id.set("locationCode", "00")
+    pick_waveform_id.set("channelCode", str(channel))
+    pick_evaluation_status = ET.SubElement(new_pick, "evaluationStatus")
+    pick_evaluation_status.text = "final"
+
+    new_arrival = ET.Element("arrival")
+    arrival_pick_id = ET.SubElement(new_arrival, "pickID")
+    arrival_pick_id.text = str(pick_id)
+    arrival_phase = ET.SubElement(new_arrival, "phase")
+    arrival_phase.text = str(phase)
+
+    return new_pick, new_arrival
+
+
+def update_xml(ssh_client, picks, event_id):
     with ssh_client.open_sftp() as sftp_client:
         with sftp_client.open(xml_file_path, mode="r") as remote_file:
             prefix = "http://geofon.gfz-potsdam.de/ns/seiscomp3-schema/0.11"
@@ -35,18 +64,45 @@ def update_xml(ssh_client, picks):
             xml_picks = root.find(f"{{{prefix}}}EventParameters").findall(f"{{{prefix}}}pick")
 
             for pick in picks:
-                pick_id_regexp = r'^' + re.escape(pick["pickIdStart"]) + r'.*' + re.escape(
-                    pick["pickIdEnd"]) + r'\.[A-Z]{3}$'
+                pick_id_regexp = r'^' + re.escape(pick["pickId"]) + r'\.[A-Z]{3}$'
+                is_xml_pick_exist = False
                 for xml_pick in xml_picks:
                     if re.match(pick_id_regexp, xml_pick.get("publicID")):
                         xml_pick.find(f"{{{prefix}}}time").find(f"{{{prefix}}}value").text = str(pick["time"])
+                        is_xml_pick_exist = True
+
+                if is_xml_pick_exist:
+                    pick_hhz, arrival_hhz = create_new_xml_pick(f"{pick.pickId}.HHZ",
+                                                            pick["phase"],
+                                                            pick["time"], pick["network"],
+                                                            pick["station"], "HHZ")
+                    pick_hhn, arrival_hhn = create_new_xml_pick(f"{pick.pickId}.HHN",
+                                                            pick["phase"],
+                                                            pick["time"], pick["network"],
+                                                            pick["station"], "HHN")
+                    pick_hhe, arrival_hhe = create_new_xml_pick(f"{pick.pickId}.HHE",
+                                                            pick["phase"],
+                                                            pick["time"], pick["network"],
+                                                            pick["station"], "HHE")
+                    pick_parent_element = root.find(f"{{{prefix}}}EventParameters")
+                    pick_parent_element.insert(0, pick_hhz)
+                    pick_parent_element.insert(0, pick_hhn)
+                    pick_parent_element.insert(0, pick_hhe)
+
+                    arrival_parent_element = root.find(f"{{{prefix}}}origin")
+                    arrival_parent_element.insert(0, arrival_hhz)
+                    arrival_parent_element.insert(0, arrival_hhn)
+                    arrival_parent_element.insert(0, arrival_hhe)
 
         with sftp_client.open(xml_file_path, mode="w") as remote_file:
             tree.write(remote_file, encoding='UTF-8', xml_declaration=True)
 
 
 def write_xml_to_db(ssh_client):
+    add_xml_command = f"scdispatch -i {xml_file_path} -O add"
     update_xml_command = f"scdispatch -i {xml_file_path} -O update"
+    stdin, stdout, stderr = ssh_client.exec_command(add_xml_command)
+    stdout.channel.recv_exit_status()
     stdin, stdout, stderr = ssh_client.exec_command(update_xml_command)
     stdout.channel.recv_exit_status()
 
@@ -68,7 +124,7 @@ def update_pick_times():
 
         dump_xml_from_db(ssh_client, event_id)
 
-        update_xml(ssh_client, picks)
+        update_xml(ssh_client, picks, event_id)
 
         write_xml_to_db(ssh_client)
 
